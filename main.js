a4440fcd5062995a05679e9b93d2e3fc1dc0bbb6
Isabela Ramos <isamrteixeira@gmail.com>
2024-01-09 01:11:35 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const nameElement = document.querySelector('#name');
+  const usernameElement = document.querySelector('#username');
+  const avatarElement = document.querySelector('#avatar');
+  const repositoriesElement = document.querySelector('#repos');
+  const followersElement = document.querySelector('#followers');
+  const followingElement = document.querySelector('#following');
+  const linkElement = document.querySelector('#link');
+
+  fetch('https://api.github.com/users/isabelaramoss')
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      avatarElement.src = json.avatar_url;
+      nameElement.innerText = json.name;
+      usernameElement.innerText = json.login;
+      followingElement.innerText = json.following;
+      followersElement.innerText = json.followers;
+      repositoriesElement = json.public_repos;
+      linkElement.href = json.html_url;
+    })
+})
+
